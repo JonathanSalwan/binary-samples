718e627bb9cf54b85acdae48cf09a9a99bfd3db4
Emmanuel Fleury <emmanuel.fleury@labri.fr>
2014-06-08 10:39:19 +0200

    Adding (real) self-modifying code example for i386

---
+++ self-modifying_code/self-modifying-32.s
@@ -0,0 +1,19 @@
+.globl _start
+	
+.text
+_start:
+	call	.+0x24
+	popl	%eax
+	movb	$0xeb, 0xe(%eax)
+	inc	%ebx
+	movb	$0xeb, 0x6(%eax)
+	movb	$0x08, 0x7(%eax)
+	push	%eax
+	cmc
+	inc	%edx
+	push	%ecx
+	dec	%ebx
+	mov	$1, %eax
+	mov	$0, %ebx
+	int	$0x80
+	jmp	.-0x1f
